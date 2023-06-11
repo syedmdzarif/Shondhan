a6f198c967c19b3cfa4c8ff3bd954aae13e2fe69
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-12 00:16:41 +0600

    Homepage with css commit by Zarif

--- index.php
+++ index.php
@@ -1,11 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shondhan</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    <!-- <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/> -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+
+   
+<title>Shondhan</title>
 </head>
 <body>
-    
+    <header id="main-header">
+        <div class="container">
+            <h1>Shondhan</h1>
+        </div>
+    </header>
+
+
+    <nav id="navbar">
+        <div class="container">
+            <ul>
+                <a class="w_login" href="jobs.php">Jobs</a>
+                <a class="e_login" href="courses.php">Courses</a>
+                <a class="w_signup" href="login_admin.php">Admin</a>
+                <a class="e_signup" href="signup.php">Signup</a>
+            </ul>
+        </div>
+    </nav>
+
+
+
+
+    <!-- carousel -->
+
+
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="images/chef.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="images/driver.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="images/gardener.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="images/delivery.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="images/slide5.jpg" class="d-block w-100" alt="...">
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden"></span>
+        </button>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- carousel end -->
+
+    <br>
+
+<marquee style= "font-size: 12px; font-family: system-ui;">
+    <p style="backgroundColor :white;">
+    "I have found my dream job on Shondhan.com, and I could not recommend it more!" - Md. Nazif Hossain *** "Shondhan makes it easy to find suitable employees for my startup" - Mr. Qadir Ali, CEO, Pran Industries LTD. *** "The courses enlisted here are amazing, all the best for the future to the Shondhan team!" - Sadia Haque
+    </p>
+</marquee>
+
+<footer>
+    Shondhan
+    <br>
+    All Rights Reserved 2023
+</footer>
+
+<!-- script for messenger -->
+
+<script src="//code.tidio.co/lwti145od7qa7ywsah1mwzznor3txctr.js" async></script>
+
 </body>
 </html>
