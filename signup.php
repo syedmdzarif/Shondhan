d574f8260ea7e0c01ec378bb036222bd1494d9b6
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-12 01:13:30 +0600

    Sign up commit by Zarif

---
+++ signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport:" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/signup.css">
+<title>Shondhan</title>
+
+</head>
+<body>
+    
+<div class="center">
+  <table >
+    <tr>
+      <td>
+        <a href="signup_worker.php" ><h2><text style="color:#666666">Job Seeker</text></h2></a>
+      </td>
+      <td>
+        <a href="login_worker.php"><text style="color:#666666"><b>Log In As Job Seeker Here </b><p>Lorem Ipsum printer took a galley. It has survived not only, but also the leap into electronic typesetting, remaining essentially unchanged. </p></text></a>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <a href="signup_conpany.php" ><h2><text style="color:#666666">Company</h2></text></a>
+      </td>
+      <td>
+        <a href="login_company.php"><text style="color:#666666"><b>Log In As Company Here </b><p>Lorem Ipsum printer took a galley. It has survived not only, but also the leap into electronic typesetting, remaining essentially unchanged. </p> </text></a>  
+      </td>
+    </tr>
+  </table>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/
+jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/
+popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
